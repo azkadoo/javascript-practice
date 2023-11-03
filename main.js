5d6a8dfe4eb86fcc6723c6e1e11e6bd34a29ae70
azkadoo <muhammadazkanurazhim@gmail.com>
2023-11-04 03:26:11 +0700

    array data mapping

--- main.js
+++ main.js
@@ -45,3 +45,79 @@ function onHover() {
 function animasiOut() {
   newText2.style.background = "aqua";
 }
+
+//------------------ array ------------------
+
+const arraySaya = ["🥑", "🍑", "🍐", "🍉", "🍊"];
+console.log(arraySaya);
+const semangka = arraySaya.includes("🍉");
+
+if (semangka) {
+  const posisiSemangka = arraySaya.indexOf("🍉");
+  const indexBefore = posisiSemangka - 1;
+  const indexAfter = posisiSemangka + 1;
+  const buahBefore = arraySaya[indexBefore];
+  const buahAfter = arraySaya[indexAfter];
+  console.log(`buah sebelum semangka adalah ${buahBefore}`);
+  console.log(`buah setelah semangka adalah ${buahAfter}`);
+  const buahAwal = arraySaya.shift();
+  console.log(`buah awal adalah ${buahAwal}`);
+  const buahAkhir = arraySaya.pop();
+  console.log(`buah akhir adalah ${buahAkhir}`);
+} else {
+  console.log("saya tidak tahu posisi semangkanya");
+}
+
+//array SHALLOW COPY / DEEP COPY
+
+const arraySaya2 = [
+  "🍌",
+  10,
+  {
+    tomato: function () {
+      console.log("ini tomat");
+    },
+  },
+  ["eat", "food"],
+];
+
+const newArraySaya = [...arraySaya2]; //array KW atau copyan
+// newArraySaya = arraySaya2.slice(); //array KW atau copyan
+// newArraySaya = Array.from(arraySaya2); //array KW atau copyan
+// newArraySaya = JSON.parse(JSON.stringify(arraySaya2)); //array KW atau copyan
+// newArraySaya[0] = "❌";
+
+arraySaya2[2].tomato();
+
+//------------------ array mapping ------------------
+const datas = [
+  {
+    name: "anya",
+    role: "vocalist",
+    age: "17",
+  },
+  {
+    name: "tengku",
+    role: "lead guitar",
+    age: "23",
+  },
+  {
+    name: "azka",
+    role: "rythm",
+    age: "21",
+  },
+  {
+    name: "adit",
+    role: "bassist",
+    age: "22",
+  },
+  {
+    name: "samuel",
+    role: "drummer",
+    age: "21",
+  },
+];
+
+datas.map((values, index) => {
+  console.log(values.name, values.role);
+});
